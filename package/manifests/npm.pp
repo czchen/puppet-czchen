1ca4b7e15d103961274ebebf2f5dec45614489c3
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-02-18 23:41:48 +0800

    Update npm

--- package/manifests/npm.pp
+++ package/manifests/npm.pp
@@ -2,28 +2,38 @@ define npm($ensure = installed) {
     case $ensure {
         installed: {
             exec { "npm install -g --prefix ${role::home}/.local ${name}":
-                path    => $role::path,
-                user    => $role::user,
-                group   => $role::group,
-                umask   => $role::umask,
-                require => Package['npm'],
+                path        => $role::path,
+                user        => $role::user,
+                group       => $role::group,
+                umask       => $role::umask,
+                environment => [
+                    "HOME=${role::home}",
+                ],
+                onlyif      => "npm list -g ${name}|grep '(empty)'",
+                require     => Package[$package::npm::npm],
             }
         }
 
         default: {
             exec { "npm install -g --prefix ${role::home}/.local ${name}@${ensure}":
-                path    => $role::path,
-                user    => $role::user,
-                group   => $role::group,
-                umask   => $role::umask,
-                require => Package['npm'],
+                path        => $role::path,
+                user        => $role::user,
+                group       => $role::group,
+                umask       => $role::umask,
+                environment => [
+                    "HOME=${role::home}",
+                ],
+                onlyif      => "npm list -g ${name}|grep '(empty)'",
+                require     => Package[$package::npm::npm],
+
             }
         }
     }
 }
 
 class package::npm {
-    package {'npm':
+    $npm = 'npm'
+    package {$npm:
         ensure => installed
     }
 
