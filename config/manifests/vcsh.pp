5160c4c3e45037371ea192ec8af706a8bc959140
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-05-22 22:06:52 +0800

    Add rpm configuration

--- config/manifests/vcsh.pp
+++ config/manifests/vcsh.pp
@@ -24,6 +24,7 @@ class config::vcsh {
         'npm'            => { url => 'https://github.com/czchen/npm.vcsh', },
         'python'         => { url => 'https://github.com/czchen/python.vcsh', },
         'rime'           => { url => 'https://github.com/czchen/rime.vcsh', },
+        'rpm'            => { url => 'https://github.com/czchen/rpm.vcsh', } ,
         'sublime-text-3' => { url => 'https://github.com/czchen/sublime-text-3.vcsh', },
         'tmux'           => { url => 'https://github.com/czchen/tmux.vcsh', },
         'vim'            => { url => 'https://github.com/czchen/vim.vcsh', },
