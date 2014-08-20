dae12efc825c71ef92c68c5e5006b282ce763a89
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2014-08-20 19:39:18 +0800

    Add vcsh repo for ssh

--- config/manifests/vcsh.pp
+++ config/manifests/vcsh.pp
@@ -27,6 +27,7 @@ class config::vcsh {
         'python'         => { url => 'https://github.com/czchen/python.vcsh', },
         'rime'           => { url => 'https://github.com/czchen/rime.vcsh', },
         'rpm'            => { url => 'https://github.com/czchen/rpm.vcsh', } ,
+        'ssh'            => { url => 'https://github.com/czchen/ssh.vcsh', } ,
         'sublime-text-3' => { url => 'https://github.com/czchen/sublime-text-3.vcsh', },
         'tmux'           => { url => 'https://github.com/czchen/tmux.vcsh', },
         'vim'            => { url => 'https://github.com/czchen/vim.vcsh', },
